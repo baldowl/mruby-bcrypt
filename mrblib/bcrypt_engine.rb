7f2ff533e14bb16edb1f80d64572ca9b09ab2d33
Emanuele Vicentini <emanuele.vicentini@gmail.com>
2016-01-23 10:12:49 +0100

    Improved BCrypt::Engine.calibrate.

--- mrblib/bcrypt_engine.rb
+++ mrblib/bcrypt_engine.rb
@@ -19,28 +19,22 @@ module BCrypt
       cost == 0 || (MIN_COST..MAX_COST).include?(cost)
     end
 
-    # MAYBE: what about raising an exception if it's invalid?
     def self.cost=(cost)
       @cost = valid_cost?(cost) ? cost.to_i : DEFAULT_COST
     end
 
-    # MAYBE: what about raising an exception if it's invalid?
     def self.generate_salt(cost = self.cost)
       cost = valid_cost?(cost) ? cost.to_i : DEFAULT_COST
       __bc_salt '$2b$', cost, __bc_random_bytes(RANDOM_BYTES)
     end
 
-    # FIXME: ugly! An easy fix would be to make gerate_salt use MIN_COST
-    # instead of DEFAULT_COST and start from 1 (not 0 because it allows the
-    # implementation to choose a default cost equals to 5).
     def self.calibrate upper_time_limit_in_ms
-      previous_cost = MIN_COST
-      (MIN_COST..MAX_COST).each do |i|
+      (MIN_COST..MAX_COST).each do |current_cost|
         start_time = Time.now
-        Password.create 'plain text password', :cost => i
+        Password.create 'plain text password', :cost => current_cost
         elapsed_time = (Time.now - start_time) * 1000
+        previous_cost = current_cost > MIN_COST ? current_cost - 1 : current_cost
         return previous_cost if elapsed_time > upper_time_limit_in_ms
-        previous_cost = i
       end
     end
   end
